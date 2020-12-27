fd250d54b1e328974d07a35174c5c2d961bfc704
like <980650920@qq.com>
2020-12-27 14:41:16 +0800

    二叉堆 top n 问题

--- 代码/src/main/java/数据结构和算法/数据结构/heap/BinaryHeap.java
+++ 代码/src/main/java/数据结构和算法/数据结构/heap/BinaryHeap.java
@@ -14,23 +14,49 @@ public class BinaryHeap<E> extends AbstractHeap<E> implements Heap<E>, BinaryTre
     public static final int DEFAULT_CAPACITY = 10;
     public E[] elements;
 
-
     public BinaryHeap() {
-        this(null);
+        this(DEFAULT_CAPACITY, null);
     }
 
-    public BinaryHeap(Comparator<E> comparator) {
-        this(DEFAULT_CAPACITY, comparator);
-    }
-
-    public BinaryHeap(int size, Comparator<E> comparator) {
-        if (size < DEFAULT_CAPACITY) size = DEFAULT_CAPACITY;
+    public BinaryHeap(int capacity, Comparator<E> comparator) {
+        if (capacity < DEFAULT_CAPACITY) capacity = DEFAULT_CAPACITY;
         this.comparator = comparator;
-        this.elements = (E[]) new Object[size];
+        this.elements = (E[]) new Object[capacity];
     }
 
-    public BinaryHeap(int size) {
-        this(size, null);
+    public BinaryHeap(int capacity) {
+        this(capacity, null);
+    }
+
+    public BinaryHeap(E[] elements) {
+        this(elements, null);
+    }
+
+    public BinaryHeap(E[] elements, Comparator<E> comparator) {
+        if (elements == null || elements.length == 0) {
+            elements = (E[]) new Object[DEFAULT_CAPACITY];
+        }
+        this.elements = (E[]) new Object[Math.max(DEFAULT_CAPACITY, elements.length)];
+        size = elements.length;
+        for (int i = 0; i < elements.length; i++) {
+            this.elements[i] = elements[i];
+        }
+        heapify();
+        this.comparator = comparator;
+    }
+
+    public BinaryHeap<E> topN(int n) {
+        if (n > size) return null;
+        BinaryHeap<E> h = new BinaryHeap<>();
+        for (E e : elements) {
+            if (h.size < n) {   // 先存放 前n个数
+                h.add(e);
+            } else if (e != null && compare(e, h.get()) > 0) {
+                // 如果 遍历的元素 大于 我们堆中的堆顶，就替换
+                h.replace(e);
+            }
+        }
+        return h;
     }
 
     /**
@@ -56,31 +82,6 @@ public class BinaryHeap<E> extends AbstractHeap<E> implements Heap<E>, BinaryTre
 
         elements[size++] = element;
         siftUp(size - 1);// 让添加的节点上滤
-    }
-
-    private void resize(int i) {
-
-    }
-
-    /**
-     * 上滤
-     *
-     * @param index 指数
-     */
-    private void siftUp(int index) {
-        E e = elements[index];  // 需要上滤的节点
-        while (index > 0) {
-            int pIndex = (index - 1) / 2; // 父节点的索引
-            E p = elements[pIndex];      // 父节点
-            if (compare(e, p) <= 0) break;  // 小于等于父节点
-
-            // e > p
-            elements[index] = p;
-
-            // 记录 e最后的位置
-            index = pIndex;
-        }
-        elements[index] = e;
     }
 
     /**
@@ -113,26 +114,6 @@ public class BinaryHeap<E> extends AbstractHeap<E> implements Heap<E>, BinaryTre
         return removeNode;
     }
 
-    private void siftDown(int index) {
-        E e = elements[index];
-        int half = size >> 1; // 第一个叶子节点的索引 = 非叶子节点的数量 index * 2 <= size || (index * 2) <= size - 1
-        while (index < half) {  // 有子节点才进入
-            // 找出最大的子节点
-            int cIndex = (index << 1) + 1;
-            E c = elements[cIndex];
-            int crIndex = cIndex + 1;
-            if (crIndex < size && compare(elements[crIndex], c) > 0) {
-                c = elements[cIndex = crIndex];
-            }
-
-            // 交换e，c
-            if (compare(e, c) >= 0) break;
-            elements[index] = c;
-            index = cIndex;
-        }
-        elements[index] = e;
-    }
-
     /**
      * 删除堆顶元素的同时插入一个新元素
      *
@@ -152,12 +133,6 @@ public class BinaryHeap<E> extends AbstractHeap<E> implements Heap<E>, BinaryTre
         }
         return remove;
     }
-
-
-    private void elementNollPointEx(E element) {
-        if (element == null) { throw new RuntimeException("不能添加null 元素");}
-    }
-
 
     @Override
     public Object root() {
@@ -179,5 +154,70 @@ public class BinaryHeap<E> extends AbstractHeap<E> implements Heap<E>, BinaryTre
     @Override
     public Object string(Object node) {
         return elements[((Integer) node)];
+    }
+
+    private void elementNollPointEx(E element) {
+        if (element == null) { throw new RuntimeException("不能添加null 元素");}
+    }
+
+    /**
+     * 批量建堆
+     * 1.自上而下的 上滤
+     * 2.自下而上的 下滤
+     */
+    private void heapify() {
+        // 1.自上而下的 上滤
+        //        for (int i = 0; i < size; i++) {
+        //            siftUp(i);
+        //        }
+        // 2.自下而上的 下滤
+        for (int i = (size >> 1) - 1; i >= 0; i--) {
+            siftDown(i);
+        }
+    }
+
+    private void resize(int i) {
+
+    }
+
+    private void siftDown(int index) {
+        E e = elements[index];
+        int half = size >> 1; // 第一个叶子节点的索引 = 非叶子节点的数量 index * 2 <= size || (index * 2) <= size - 1
+        while (index < half) {  // 有子节点才进入
+            // 找出最大的子节点
+            int cIndex = (index << 1) + 1;
+            E c = elements[cIndex];
+            int crIndex = cIndex + 1;
+            if (crIndex < size && compare(elements[crIndex], c) > 0) {
+                c = elements[cIndex = crIndex];
+            }
+
+            // 交换e，c
+            if (compare(e, c) >= 0) break;
+            elements[index] = c;
+            index = cIndex;
+        }
+        elements[index] = e;
+    }
+
+    /**
+     * 上滤
+     *
+     * @param index 指数
+     */
+    private void siftUp(int index) {
+        E e = elements[index];  // 需要上滤的节点
+        while (index > 0) {
+            int pIndex = (index - 1) / 2; // 父节点的索引
+            E p = elements[pIndex];      // 父节点
+            if (compare(e, p) <= 0) break;  // 小于等于父节点
+
+            // e > p
+            elements[index] = p;
+
+            // 记录 e最后的位置
+            index = pIndex;
+        }
+        elements[index] = e;
     }
 }
--- 代码/src/main/java/数据结构和算法/数据结构/heap/Main.java
+++ 代码/src/main/java/数据结构和算法/数据结构/heap/Main.java
@@ -2,8 +2,6 @@ package 数据结构和算法.数据结构.heap;
 
 import 数据结构和算法.数据结构.tree.printer.BinaryTrees;
 
-import java.util.Arrays;
-
 /**
  * @author like
  * @date 2020-12-27 11:00
@@ -13,16 +11,10 @@ import java.util.Arrays;
 public class Main {
 
     public static void main(String[] args) {
-        BinaryHeap<Integer> h = new BinaryHeap<>();
         Integer[] list = new Integer[]{
                 68, 72, 43, 50, 38,100,23
         };
-        for (Integer i : list) {
-            h.add(i);
-        }
-        BinaryTrees.println(h);
-        h.replace(11);
-        BinaryTrees.println(h);
-        System.out.println(Arrays.toString(h.elements));
+        BinaryHeap<Integer> h = new BinaryHeap<>(list);
+        BinaryTrees.println(h.topN(3));
     }
 }
