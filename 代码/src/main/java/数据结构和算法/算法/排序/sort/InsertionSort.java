627ef83a0726d8708a596404c4ad48855f7ecde9
like <980650920@qq.com>
2020-12-30 09:23:44 +0800

    【20201230】】插入排序小优化

--- 代码/src/main/java/数据结构和算法/算法/排序/sort/InsertionSort.java
+++ 代码/src/main/java/数据结构和算法/算法/排序/sort/InsertionSort.java
@@ -10,6 +10,23 @@ public class InsertionSort<E extends Comparable<E>> extends Sort<E> {
 
     @Override
     protected E[] sort() {
+        return m2();
+    }
+
+    private E[] m2() {
+        for (int start = 1; start < array.length; start++) {        // 1.假设排好序的数组是 array[0]
+            int cur = start;                                       // 2.记录取出元素的index
+            E unSort = array[cur];                                // 3.记录取出未排序数组中的元素
+            while (cur > 0 && cmp(unSort, array[cur - 1]) <0){   // 4.比较未排序元素，和已经排序的元素
+                array[cur] = array[cur - 1];                    // 5.如果小于就一直交换，直到大于
+                cur--;
+            }
+            array[cur] = unSort;                             // 6.最终排序的位置
+        }
+        return array;
+    }
+
+    private E[] m1() {
         for (int start = 1; start < array.length; start++) {   // 1.假设排好序的数组是 array[0]
             int cur = start;                                  // 2.记录取出元素的index
             while (cur > 0 && cmp(cur, cur - 1) < 0) {       // 3.比较 array[cur] 是不是小于 array[cur-1]
