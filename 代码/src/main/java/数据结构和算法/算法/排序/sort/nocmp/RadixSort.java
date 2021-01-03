423d2bce6d4e69d10a54afe0ecc29812691c6bcd
like <980650920@qq.com>
2021-01-03 12:51:25 +0800

    基数排序-未完成

---
+++ 代码/src/main/java/数据结构和算法/算法/排序/sort/nocmp/RadixSort.java
@@ -0,0 +1,42 @@
+package 数据结构和算法.算法.排序.sort.nocmp;
+
+import 数据结构和算法.算法.排序.sort.Sort;
+
+/**
+ * @author like
+ * @date 2021-01-03 12:14
+ * @contactMe 980650920@qq.com
+ * @description
+ */
+public class RadixSort extends Sort<Integer> {
+
+
+    @Override
+    protected Integer[] sort() {
+        int max = array[0];
+        for (Integer integer : array) {
+            if (integer > max) {
+                max = integer;
+            }
+        }
+        for (int i = 0; i <= max; i *= 10) {
+            countSort(i);
+        }
+        return null;
+    }
+
+    private void countSort(int d) {
+        // 2.写一个序列，保存0-max的所有值
+        int[] counts = new int[10];
+        for (Integer i : array) {
+            counts[i]++;
+        }
+        // 3.给数组重新赋值
+        int idx = 0;
+        for (int i = 0; i < counts.length; i++) {
+            while (counts[i]-- != 0) {
+                array[idx++] = i;
+            }
+        }
+    }
+}
