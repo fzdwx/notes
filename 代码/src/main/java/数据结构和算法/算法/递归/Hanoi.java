7a3341dfa00da52ba73da66b65f5f04af52ad8bb
like <980650920@qq.com>
2021-01-15 09:12:27 +0800

    哈诺塔

---
+++ 代码/src/main/java/数据结构和算法/算法/递归/Hanoi.java
@@ -0,0 +1,27 @@
+package 数据结构和算法.算法.递归;
+
+/**
+ * @author like
+ * @date 2021-01-14 14:43
+ * @contactMe 980650920@qq.com
+ * @description
+ */
+public class Hanoi {
+
+    public static void main(String[] args) {
+        hanoi(1,"A", "B", "C");
+    }
+
+    public static void hanoi(int n, String a, String center, String c) {
+        if (n == 1) {
+            move(n, a,c); return;
+        }
+        hanoi(n - 1, a, c, center);
+        move(n, a,c);
+        hanoi(n-1,center, a, c);
+    }
+
+    private static void move(int n,String from,String to) {
+        System.out.println("将"+n+"号盘子从"+from+"移动到"+to);
+    }
+}
