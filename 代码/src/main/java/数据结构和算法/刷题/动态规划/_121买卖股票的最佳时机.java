018f2a7eb81f47c19a0b56ff3d1396ecbe0e3310
like <980650920@qq.com>
2021-01-27 14:34:15 +0800

    update

--- 代码/src/main/java/数据结构和算法/刷题/动态规划/_121买卖股票的最佳时机.java
+++ 代码/src/main/java/数据结构和算法/刷题/动态规划/_121买卖股票的最佳时机.java
@@ -1,5 +1,7 @@
 package 数据结构和算法.刷题.动态规划;
 
+import java.util.Arrays;
+
 /**
  * @author like
  * @date 2021-01-27 12:42
@@ -9,8 +11,19 @@ package 数据结构和算法.刷题.动态规划;
 public class _121买卖股票的最佳时机 {
     public static void main(String[] args) {
         solution(new int[]{
-               7,1,5,3,6,4
+                7, 1, 5, 3, 6, 4
         });
+    }
+
+
+    public static int solution2(int[] arr) {
+        if (arr == null || arr.length <= 0) return -1;
+        int[] dp = new int[arr.length];
+        for (int i = 1; i < arr.length; i++) {
+            dp[i - 1] = arr[i] - arr[i - 1];
+        }
+        System.out.println(Arrays.toString(dp));
+        return 0;
     }
 
     public static int solution(int[] arr) {
