e2595291d9ac8651927add7ef34a185392347e82
like <980650920@qq.com>
2021-02-28 16:25:36 +0800

    hashMap demo

---
+++ 代码/Interview/src/main/java/com/like/interview/thirdEpisode/HashMapDemo.java
@@ -0,0 +1,20 @@
+package com.like.interview.thirdEpisode;
+
+import java.util.HashMap;
+
+/**
+ * @author like
+ * @email 980650920@qq.com
+ * @since 2021-02-28 15:08
+ */
+public class HashMapDemo {
+
+    public static void main(String[] args) {
+        HashMap<String, String> map = new HashMap<>();
+        map.put("like", "test");
+        map.put("keke", "test");
+        map.put("lili", "test");
+        map.put("ccc", "test");
+        map.put("zzz", "test");
+    }
+}
