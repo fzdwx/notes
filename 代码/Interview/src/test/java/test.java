64d40abdd53f956a65e9d4572166d4bf4967e53c
like <980650920@qq.com>
2021-03-03 09:36:23 +0800

    笔记更新

--- 代码/Interview/src/test/java/test.java
+++ 代码/Interview/src/test/java/test.java
@@ -6,18 +6,41 @@ import java.util.Arrays;
  * @since 2021-02-28 20:38
  */
 public class test {
-    public static void main(String[] args) {
-        int[] arr = {3, 4, 1, 2, 5, 6};
+    static int[] arr = {3, 4, 1, 2, 5, 6};
+    static int[] lArr;
 
-        for (int i = 0; i < arr.length - 1; i++) {
-            for (int j = i; j < arr.length; j++) {
-                if (arr[i] > arr[j]) {
-                    int temp = arr[j];
-                    arr[j] = arr[i];
-                    arr[i] = temp;
-                }
-            }
-        }
+    public static void main(String[] args) {
+        lArr = new int[arr.length >> 1];
+        divide(0, arr.length);
         System.out.println(Arrays.toString(arr));
     }
+
+    private static void divide(int begin, int end) {
+        if (end - begin < 2) {
+            return;
+        }
+        int mid = (begin + end) >> 1;
+        divide(begin, mid);
+        divide(mid, end);
+        merge(begin, mid, end);
+    }
+
+    private static void merge(int begin, int mid, int end) {
+        int l1 = 0, l2 = mid - begin;
+        int r1 = mid;
+        int a1 = begin;
+
+        for (int i = l1; i < l2; i++) {
+            lArr[i] = arr[i + begin];
+        }
+        while (l1 < l2) {
+            if (r1 < end && arr[r1] < lArr[l1]) {
+                arr[a1++] = arr[r1++];
+            } else {
+                arr[a1++]=lArr[l1++];
+            }
+        }
+    }
+
+
 }
