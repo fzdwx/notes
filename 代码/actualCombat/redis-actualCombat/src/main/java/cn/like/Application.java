5ceb9cef66e43ace813b93fcf56964b41637c0cd
like <980650920@qq.com>
2021-05-02 12:24:52 +0800

    redis 主从架构搭建

---
+++ 代码/actualCombat/redis-actualCombat/src/main/java/cn/like/Application.java
@@ -0,0 +1,25 @@
+package cn.like;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * .____    .__ __
+ * |    |   |__|  | __ ____
+ * |    |   |  |  |/ // __ \
+ * |    |___|  |    <\  ___/
+ * |_______ \__|__|_ \\___  >
+ * \/       \/    \/
+ *
+ * redis 实战
+ *
+ * @author like
+ * @since 2021-04-29 20:51
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
