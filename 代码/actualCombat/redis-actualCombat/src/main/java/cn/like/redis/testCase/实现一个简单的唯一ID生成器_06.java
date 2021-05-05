08ed8af03b1b74331cc5e8a73b659462dc5cc7be
like <980650920@qq.com>
2021-05-05 17:10:37 +0800

    实现一个简单的唯一ID生成器

---
+++ 代码/actualCombat/redis-actualCombat/src/main/java/cn/like/redis/testCase/实现一个简单的唯一ID生成器_06.java
@@ -0,0 +1,23 @@
+package cn.like.redis.testCase;
+
+import static cn.like.redis.testCase.Redis.cmd;
+
+/**
+ * .____    .__ __
+ * |    |   |__|  | __ ____
+ * |    |   |  |  |/ // __ \
+ * |    |___|  |    <\  ___/
+ * |_______ \__|__|_ \\___  >
+ * \/       \/    \/
+ * <p>
+ * desc
+ *
+ * @author like
+ * @date 2021-05-05 17:06
+ */
+public class 实现一个简单的唯一ID生成器_06 {
+
+    public static void main(String[] args) {
+        cmd().incr("test");
+    }
+}
