c618bbcce1e0f0d2b17a00e53b02b14b9a6d8e50
like <980650920@qq.com>
2021-05-19 19:55:02 +0800

    geo 案例

---
+++ 代码/actualCombat/redis-actualCombat/src/main/java/cn/like/redis/testCase/geo/基于GeoHash的你与商铺距离计算的程序.java
@@ -0,0 +1,45 @@
+package cn.like.redis.testCase.geo;
+
+import io.lettuce.core.GeoArgs;
+
+import static cn.like.redis.testCase.Redis.sync;
+
+/**
+ * @author: like
+ * @since: 2021/5/19 19:37
+ * @email: 980650920@qq.com
+ * @desc:
+ */
+public class 基于GeoHash的你与商铺距离计算的程序 {
+
+    /**
+     * 添加位置
+     *
+     * @param name      的名字
+     * @param longitude 经度
+     * @param latitude  纬度
+     */
+    public void addLocation(String name, double longitude, double latitude) {
+        sync().geoadd("location_date", longitude, latitude, name);
+    }
+
+    /**
+     * 得到距离
+     *
+     * @param from 从
+     * @param to   来
+     * @return {@link Double}
+     */
+    public Double getDist(String from, String to) {
+        return sync.geodist("location_date", from, to, GeoArgs.Unit.km);
+    }
+
+    public static void main(String[] args) {
+        基于GeoHash的你与商铺距离计算的程序 test = new 基于GeoHash的你与商铺距离计算的程序();
+
+        test.addLocation("天安门广场", 116.39777173016356, 39.90368886319734);
+        test.addLocation("我", 116.46132935498045, 39.90599332133357);
+
+        System.out.println(test.getDist("天安门广场", "我"));
+    }
+}
