71454b69fe438c1d7158656f559839b1c6876909
like <980650920@qq.com>
2021-06-08 09:54:02 +0800

    1.多线程问题引入

--- javaBasis/java-util-concurrent/src/main/java/cn/like/code/concurrent/case_1/ThreadUnsafeExample.java
+++ javaBasis/java-util-concurrent/src/main/java/cn/like/code/concurrent/case_1/ThreadUnsafeExample.java
@@ -1,16 +1,12 @@
 package cn.like.code.concurrent.case_1;
 
 
-import cn.hutool.poi.excel.cell.CellEditor;
-import io.vavr.Function2;
-import io.vavr.Tuple;
-import io.vavr.Tuple2;
-import io.vavr.collection.List;
-
-import java.util.Arrays;
-import java.util.function.Function;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.Executors;
 
 /**
+ * desc: 多线程问题引入
+ *
  * @author like
  * @date 2021/6/7 16:21
  */
@@ -22,27 +18,35 @@ public class ThreadUnsafeExample {
         this.cnt++;
     }
 
-    public int getCnt() {
+    public int cnt() {
         return cnt;
     }
 
-    public static void main(String[] args) {
-        String str = """
-                123
-                """;
-        final Tuple2<String, Integer> java8 = Tuple.of("Java", 8);
+    public void cnt(int cnt) {
+        this.cnt = cnt;
+    }
 
-        final Tuple2<String, Integer> map = java8.map(s -> s.substring(2) + "va",
-                i -> i / 8);
+    public static void main(String[] args) throws InterruptedException {
+        var example = new ThreadUnsafeExample();
+        final var threadSize = 10000; // fixed thread count
 
-        final Tuple2<char[], Integer> integerTuple2 = java8.map1(String::toCharArray);
+        var pool = Executors.newCachedThreadPool();
+        while (true) {
+            var countDownLatch = new CountDownLatch(threadSize);
 
-        final String s1 = java8.apply((s, i) -> "hello");
-        System.out.println(s1);
-        System.out.println(Arrays.toString(integerTuple2._1));
-        System.out.println(str);
-        System.out.println(map);
+            for (int i = 0; i < threadSize; i++) {
+                pool.execute(() -> {
+                    example.add();
+                    countDownLatch.countDown();
+                });
+            }
+            countDownLatch.await();
+            if (threadSize == example.cnt()) break;  // 相等就返回
 
-        final Function2<Integer,Integer,Integer> f1 = (a, b) -> a + b;
+            example.cnt(0); // reset
+        }
+        pool.shutdown();
+
+        System.out.println(example.cnt()); // 永远小于 10000
     }
 }
